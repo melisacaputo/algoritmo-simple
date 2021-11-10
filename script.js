f8ff5353125c7ac7baa3f7100cdae8ab8e486b6a
melisacaputo <meliscap@hotmail.com>
2021-11-09 22:46:04 -0300

    Desafio en GIT

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let nombre = prompt("Ingresa tu nombre");
+alert("¡Hola," + " " + nombre + "!");
+console.log ("El nombre del usuario es:" + " " + nombre);
+
+let usd = 180;
+let ars = parseFloat(prompt("Ingresa cuantos pesos deseas convertir"));
+alert("Tus pesos argentinos equivalen a $" + (usd * ars) + " USD");
